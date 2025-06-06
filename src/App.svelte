eb79bebefbb041edcb6f7cdd67a27e9b3a61f259
vitalspace <myglobalsounds@gmail.com>
2025-06-06 15:18:40 -0600

    Updated App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,7 @@
 <script>
-  import { T } from '@threlte/core'
+  import { Canvas } from '@threlte/core'
+  import Scene from './lib/Scene.svelte'
 </script>
-
-<T.PerspectiveCamera
-  makeDefault
-  position={[10, 10, 10]}
-  oncreate={(ref) => {
-    ref.lookAt(0, 1, 0)
-  }}
-/>
-
-<T.Mesh position.y={1}>
-  <T.BoxGeometry args={[1, 2, 1]} />
-  <T.MeshBasicMaterial color="hotpink" />
-</T.Mesh>
+<Canvas>
+  <Scene />
+</Canvas>
