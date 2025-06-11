1f53efc075e5ab6ae47e30abce1dcdbbc61968fd
vitalspace <myglobalsounds@gmail.com>
2025-06-10 22:22:10 -0600

    Updated Scene.svelte

--- src/lib/pages/home/Scene.svelte
+++ src/lib/pages/home/Scene.svelte
@@ -10,21 +10,14 @@
 
 <Sky elevation={-2} />
 
-<Stars/>
 
 <T.PerspectiveCamera
-  position={[0, 7, 18]}
-  fov={60}
-  near={1}
-  far={20000}
   makeDefault
->
-  <OrbitControls
-    maxPolarAngle={85 * DEG2RAD}
-    enableDamping
-    target={[0, 2.5, 0]}
-  />
-</T.PerspectiveCamera>
+  position={[10, 10, 10]}
+  oncreate={(ref) => {
+    ref.lookAt(0, 1, 0)
+  }}
+/>
 
 <T.DirectionalLight
   position={[0, 10, 10]}
