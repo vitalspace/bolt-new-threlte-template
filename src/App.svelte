8fcb0ca6cbbc50fd60dbf7e9aac80c98589e163e
vitalspace <myglobalsounds@gmail.com>
2025-06-10 22:03:21 -0600

    Updated App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,15 @@
 <script>
   import { Router, Link, Route } from "svelte5-router"
-    
+  import Home from "./routes/Home.svelte"
+  
+  
 </script>
+
+<Router>
+<!-- Menu -->
+
+  <div>
+    <Route path="/" component={Home} />
+  </div>
+
+</Router>
