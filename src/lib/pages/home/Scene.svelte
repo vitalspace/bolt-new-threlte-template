88581812beb829a4d0c81ba682f69fefcbf85376
vitalspace <myglobalsounds@gmail.com>
2025-06-10 22:15:52 -0600

    Updated Scene.svelte

--- src/lib/pages/home/Scene.svelte
+++ src/lib/pages/home/Scene.svelte
@@ -1,18 +1,10 @@
 <script>
   import { T, useTask } from '@threlte/core'
-  import { interactivity } from '@threlte/extras'
-  import { Spring } from 'svelte/motion'
   import City from "./components/world/City.svelte"
   
   
 
-  interactivity()
-
-  const scale = new Spring(1)
-
-  let rotation = 0
   useTask((delta) => {
-    rotation += delta
   })
 </script>
 
@@ -29,27 +21,4 @@
   castShadow
 />
 
-<T.Mesh
-  rotation.y={rotation}
-  position.y={1}
-  scale={scale.current}
-  onpointerenter={() => {
-    scale.target = 1.5
-  }}
-  onpointerleave={() => {
-    scale.target = 1
-  }}
-  castShadow
->
-  <T.BoxGeometry args={[1, 2, 1]} />
-  <T.MeshStandardMaterial color="hotpink" />
-</T.Mesh>
-
-<T.Mesh
-  rotation.x={-Math.PI / 2}
-  receiveShadow
->
-  <T.CircleGeometry args={[4, 40]} />
-  <T.MeshStandardMaterial color="white" />
-</T.Mesh>
 <City/>
